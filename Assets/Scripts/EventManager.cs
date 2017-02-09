4b0df141349183efec90426b1f60719f44fb2e3a
Unknown <patrickmonville@hotmail.com>
2017-02-09 15:47:04 +0100

    implemented main menu and pause menu

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void OnGameStartedDelegate();
+public delegate void OnGamePausedDelegate();
+public delegate void OnGameUnpausedDelegate();
+public delegate void OnGameEndedDelegate();
+
+public class EventManager
+{
+    private static EventManager _instance;
+
+    public static EventManager Instance { get
+        {
+            if(_instance == null)
+            {
+                _instance = new EventManager();
+            }
+
+            return _instance;
+        }
+    }
+
+    public event OnGameStartedDelegate OnGameStartedEvent;
+    public event OnGamePausedDelegate OnGamePausedEvent;
+    public event OnGameUnpausedDelegate OnGameUnpausedEvent;
+    public event OnGameEndedDelegate OnGameEndedEvent;
+
+    public void SendOnGameStartedEvent()
+    {
+        OnGameStartedDelegate tmp = OnGameStartedEvent;
+
+        if (tmp != null)
+        {
+            Debug.Log("EventManager: game started");
+            this.OnGameStartedEvent();
+        }
+    }
+
+    public void SendOnGamePausedEvent()
+    {
+        OnGamePausedDelegate tmp = OnGamePausedEvent;
+
+        if (tmp != null)
+        {
+            this.OnGamePausedEvent();
+        }
+    }
+
+    public void SendOnGameUnpausedEvent()
+    {
+        OnGameUnpausedDelegate tmp = OnGameUnpausedEvent;
+
+        if (tmp != null)
+        {
+            this.OnGameUnpausedEvent();
+        }
+    }
+
+    public void SendOnGameEndedEvent()
+    {
+        OnGameEndedDelegate tmp = OnGameEndedEvent;
+
+        if (tmp != null)
+        {
+            this.OnGameEndedEvent();
+        }
+    }
+}
---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameScript : MonoBehaviour
+{
+    protected bool GamePaused { get; private set; }
+
+    // Use this for initialization
+    void Awake()
+    {
+        Initialize();
+    }
+
+    protected void Initialize()
+    {
+        GamePaused = true;
+
+        EventManager.Instance.OnGameStartedEvent += this.OnGameStarted;
+        EventManager.Instance.OnGamePausedEvent += this.OnGamePaused;
+        EventManager.Instance.OnGameUnpausedEvent += this.OnGameUnpaused;
+        EventManager.Instance.OnGameEndedEvent += this.OnGameEnded;
+    }
+
+    protected virtual void OnGameStarted()
+    {
+        Debug.Log("game started");
+        GamePaused = false;
+    }
+
+    protected virtual void OnGamePaused()
+    {
+        GamePaused = true;
+    }
+
+    protected virtual void OnGameUnpaused()
+    {
+        GamePaused = false;
+    }
+
+    protected virtual void OnGameEnded()
+    {
+        GamePaused = true;
+    }
+}
